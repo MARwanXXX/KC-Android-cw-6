531d980b372ed8dcf2c295210c591411da99016f
MARwanXXX <wana28105@gmail.com>
2022-08-15 05:04:46 +0300

    done

--- Android School/app/src/main/java/com/example/android/myapplication/MainActivity.java
+++ Android School/app/src/main/java/com/example/android/myapplication/MainActivity.java
@@ -9,22 +9,25 @@ import android.widget.Toast;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    /**
-     *  عرفوا مصفوفة بإسم students
-     *  define an arraylist named students
-     */
+
+    ArrayList<Student> students = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         ListView listView = findViewById(R.id.studentListView);
+        Student s1 = new Student("يوسف", 14, 12,R.drawable.monkey );
+        Student s2 = new Student("سلمان", 13, 10,R.drawable.monkey );
+        Student s3 = new Student("ماجد", 15, 11,R.drawable.monkey );
+        Student s4 = new Student("بهجت", 17, 12,R.drawable.monkey );
 
-        /**
-         *  إكتبوا الكود بعد هذا السطر
-         *  ضيفوا عناصر من الكلاس
-         *  add objects from class Student
-         */
+
+        students.add(s1);
+        students.add(s2);
+        students.add(s3);
+        students.add(s4);
 
 
         StudentAdapter studentAdapter = new StudentAdapter(this,0,students);
---
+++ Android School/app/src/main/java/com/example/android/myapplication/Student.java
@@ -0,0 +1,48 @@
+package com.example.android.myapplication;
+
+public class Student {
+
+    private String StudentName;
+    private int StudentAge;
+    private int StudentGrade;
+    private int StudentImg;
+
+    public Student(String studentName, int studentAge, int studentGrade, int studentImg) {
+        StudentName = studentName;
+        StudentAge = studentAge;
+        StudentGrade = studentGrade;
+        StudentImg = studentImg;
+    }
+
+    public String getStudentName() {
+        return StudentName;
+    }
+
+    public void setStudentName(String studentName) {
+        StudentName = studentName;
+    }
+
+    public int getStudentAge() {
+        return StudentAge;
+    }
+
+    public void setStudentAge(int studentAge) {
+        StudentAge = studentAge;
+    }
+
+    public int getStudentGrade() {
+        return StudentGrade;
+    }
+
+    public void setStudentGrade(int studentGrade) {
+        StudentGrade = studentGrade;
+    }
+
+    public int getStudentImg() {
+        return StudentImg;
+    }
+
+    public void setStudentImg(int studentImg) {
+        StudentImg = studentImg;
+    }
+}
